2ab06e29b49507d345be9bd6a8fdd10065730060
Junrill Galvez <junrillg@gmail.com>
2018-06-20 13:30:11 +0800

    init

--- action.js
+++ action.js
@@ -1,9 +1,12 @@
 const sh = require('shelljs')
+const ch = require('chalk')
 
 exports.createNewFeature = (id) => {
     if (sh.which('git')) {
         // shell.echo('Sorry, this script requires git');
         // shell.exit(1);
-        sh.exec('node --version', { silent: true }).output
+        sh.exec(`git checkout master && git checkout -b feature/${id}`, () => {
+
+        })
     }
 }
